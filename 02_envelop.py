3647f449e6b08b9ff62d544b813fe07e27556b2d
shnashka <shnashkaa@mail.ru>
2021-11-22 15:35:44 +0300

    22

--- 02_envelop.py
+++ 02_envelop.py
@@ -21,21 +21,26 @@ paper_x, paper_y = 8, 9
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
-
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print('+')
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    print('+')
+else:
+    print('-')
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
+brick_x, brick_y, brick_z = 3, 5, 6
 # brick_x, brick_y, brick_z = 3, 6, 5
 # brick_x, brick_y, brick_z = 6, 3, 5
 # brick_x, brick_y, brick_z = 6, 5, 3
@@ -48,5 +53,20 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
+if hole_x >= brick_x and hole_y >= brick_y:
+    print('+')
+elif hole_x >= brick_x and hole_y >= brick_z:
+        print('+')
+elif hole_x >= brick_z and hole_y >= brick_x:
+        print('+')
+elif hole_x >= brick_z and hole_y >= brick_y:
 
+        print('+')
+elif hole_x >= brick_y and hole_y >= brick_x:
+        print('+')
+elif hole_x >= brick_y and hole_y >= brick_z:
+        print('+')
+
+else:
+    print('-')
 # TODO здесь ваш код
